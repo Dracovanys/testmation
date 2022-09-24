a97a8a7966f7141356a3a86b03e567d2191f1a1b
dracovanys <mat_de_souza@hotmail.com>
2022-09-24 15:21:15 -0400

    Command syntax error detection improved.

--- src/tool.py
+++ src/tool.py
@@ -13,65 +13,115 @@ def sendCommand(command:str, result:list, result_folder:str):
     '''
     Different treatment for each
     command.
-    '''   
+    '''
 
     sentCommand = "adb "
 
-    if command.find("TAP") > -1:
+    if command.find("TAP") == 0:
         x = command[command.find("(") + 1:command.find(",")].strip()
         y = command[command.find(",") + 1:command.find(")")].strip()
-        sentCommand += tap(x, y)
-        result.append("Pass")
 
-    elif command.find("SWIPE") > -1:
+        # Checking parameters
+        try:
+            check = float(x)
+            check = float(y)
+        except:
+            result.append("Invalid")
+            return
+
+        sentCommand += tap(x, y)
+
+    elif command.find("SWIPE") == 0:
 
         x1_position = command.find("(") + 1
         x1 = command[x1_position:command.find(",", x1_position)].strip()
         y1_position = command.find(",", x1_position) + 1
         y1 = command[y1_position:command.find(",", y1_position)].strip()
         x2_position = command.find(",", y1_position) + 1
-        x2 = command[x2_position:command.find(",", x2_position)].strip()        
+        x2 = command[x2_position:command.find(",", x2_position)].strip()
+        y2_position = command.find(",", x2_position) + 1
+        y2 = command[y2_position:command.find(",", y2_position)].strip()       
 
         # Conditional logic in case of user don't give duration
-        if command.find(",", x2_position + (len(x2) + 2)) > -1:
-            y2_position = command.find(",", x2_position) + 1
-            y2 = command[y2_position:command.find(",", y2_position)].strip()
+        if command.find(",", y2_position + (len(y2) + 2)) > -1:            
             duration_position = command.find(",", y2_position) + 1
             duration = command[duration_position:command.find(")", duration_position)].strip()
+
+            # Checking parameters
+            try:
+                check = float(x1)
+                check = float(y1)
+                check = float(x2)
+                check = float(y2)
+                check = int(duration)
+            except:
+                result.append("Invalid")
+                return
+
+            # Authorize command
             sentCommand += swipe(x1, y1, x2, y2, duration)
+
         elif command.find(")", x2_position + (len(x2) + 2)) > -1:
             y2_position = command.find(",", x2_position) + 1
             y2 = command[y2_position:command.find(",", y2_position)].strip()
-            sentCommand += swipe(x1, y1, x2, y2)
-        
-        result.append("Pass")
 
-    elif command.find("DRAGDROP") > -1:
+            # Checking parameters
+            try:
+                check = float(x1)
+                check = float(y1)
+                check = float(x2)
+                check = float(y2)
+            except:
+                result.append("Invalid")
+                return
+            
+            # Authorize command
+            sentCommand += swipe(x1, y1, x2, y2)
+
+    elif command.find("DRAGDROP") == 0:
         x1 = command[command.find("(") + 1:command.find(",")].strip()
         y1 = command[command.find(x1) + (len(x1) + 1):command.find(",", command.find(x1) + (len(x1) + 1))].strip()
         x2 = command[command.find(y1) + (len(y1) + 1):command.find(",", command.find(y1) + (len(y1) + 1))].strip()
         y2 = command[command.find(x2) + (len(x2) + 1):command.find(",", command.find(x2) + (len(x2) + 1))].strip()
         duration = command[command.find(y2) + (len(y2) + 1):command.find(")", command.find(y2) + (len(y2) + 1))].strip()
-        sentCommand += dragAndDrop(x1, y1, x2, y2, duration)
-        result.append("Pass")
 
-    elif command.find("OPEN") > -1:
-        app = command[command.find("(") + 1:command.find(")")].strip()
-        sentCommand += openApp(app)
-        result.append("Pass")
-
-    elif command.find("COMPARE_IMAGE") > -1:
-        if not os.path.exists(f"{root}/reference"):
-            os.mkdir(f"{root}/reference")
-            result.append("Error")
+        # Checking parameters
+        try:
+            check = float(x1)
+            check = float(y1)
+            check = float(x2)
+            check = float(y2)
+        except:
+            result.append("Invalid")
             return
+
+        sentCommand += dragAndDrop(x1, y1, x2, y2, duration)
+
+    elif command.find("OPEN") == 0:
+        app = command[command.find("(") + 1:command.find(")")].strip()
+
+        # Checking parameters
+        if app.find("com.") != 0:
+            result.append("Invalid")
+            return
+
+        sentCommand += openApp(app)        
+
+    elif command.find("COMPARE_IMAGE") == 0:
 
         referenceImage = command[command.find("(") + 1:command.find(",")].strip()
-        passRate = float(command[command.find(",") + 1:command.find(")")].strip())
+        passRate = command[command.find(",") + 1:command.find(")")].strip()
 
-        if not os.path.exists(f"{root}/reference/{referenceImage}"):
-            result.append("Error")
-            return
+        # Checking parameters
+        try:
+            check = float(passRate)
+
+            if not os.path.exists(f"{root}/reference/{referenceImage}"):
+                result.append("Invalid")
+                return
+        except:
+            result.append("Invalid")
+            return        
 
         sentCommand_ = sentCommand + captureImage()
         validationImage = sentCommand_[sentCommand_.find("/", sentCommand_.find("sdcard")) + 1:]
@@ -93,6 +143,10 @@ def sendCommand(command:str, result:list, result_folder:str):
             result.append("Fail")
         return
 
+    else:
+        result.append("Invalid")
+        return
+
     # Set sleep between tests
     if command.find("{") > -1:
         sleepTime = int(command[command.find("{") + 1:command.find("}")].strip())
@@ -103,9 +157,11 @@ def sendCommand(command:str, result:list, result_folder:str):
     if command.find("[") > -1:
         for count in range(int(command[command.find("[") + 1:command.find("]")].strip())):
             os.system(sentCommand)
+            result.append("Pass")
             sleep(sleepTime)
     else:
         os.system(sentCommand)
+        result.append("Pass")
         sleep(sleepTime)
 
 def getCommands(test_case:str):
@@ -154,6 +210,11 @@ def getTestCases(test_cycle:str):
                 return testCases
 
 def execTestCycle(test_cycle:str):
+    
+    '''
+    Start and manage all test cycle
+    execution    
+    '''
 
     # Create folder for storage results
     if not os.path.exists(f"{root}/test-execution"):
@@ -184,17 +245,23 @@ def execTestCycle(test_cycle:str):
                 sys.stdout.flush()
 
         # Logging results
+
+        '''
+        PASS - All commands executed normally and verifications passed.
+        FAIL - At least, 1 command or verification failed.
+        INVALID - At least, 1 command or verification have a syntax problem.
+        '''
+
         if "Fail" in results:
             testCycle_log.append(f"[FAIL] {testCase_number} - {testCase}")
-        elif "Error" in results:
-            testCycle_log.append(f"[ERROR] {testCase_number} - {testCase}")
+        elif "Invalid" in results:
+            testCycle_log.append(f"[INVALID] {testCase_number} - {testCase}")
         else:
             testCycle_log.append(f"[PASS] {testCase_number} - {testCase}")
         
         os.system("clear")
         for log in testCycle_log:
             print(log)
-
         
         testCase_number += 1
 
