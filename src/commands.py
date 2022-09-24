2ce9abf258032444ff331337308ecadec9dce035
dracovanys <mat_de_souza@hotmail.com>
2022-09-24 02:08:28 -0400

    Compare image feature and validation process.

--- src/commands.py
+++ src/commands.py
@@ -1,18 +1,31 @@
+import os
+from datetime import datetime
+
 # Touch commands
 
 def tap(x:float, y:float):
-    return f"input tap {x} {y}"
+    return f"shell input tap {x} {y}"
 
 def swipe(x1:float, y1:float, x2:float, y2:float, duration:int=None):
     if duration == None:
-        return f"input swipe {x1} {y1} {x2} {y2} 1000"
+        return f"shell input swipe {x1} {y1} {x2} {y2} 1000"
     else:
-        return f"input swipe {x1} {y1} {x2} {y2} {duration}"
+        return f"shell input swipe {x1} {y1} {x2} {y2} {duration}"
 
 def dragAndDrop(x1:float, y1:float, x2:float, y2:float, duration:int):
-    return f"input draganddrop {x1} {y1} {x2} {y2} {duration}"
+    return f"shell input draganddrop {x1} {y1} {x2} {y2} {duration}"
 
 # System commands
 
 def openApp(app:str):
-    return f"monkey -p {app} -v 1"
+    return f"shell monkey -p {app} -v 1"
+
+# Verification commands
+
+def captureImage():
+    imageName = f"{datetime.now().day}{datetime.now().month}{datetime.now().year}{datetime.now().hour}{datetime.now().minute}{datetime.now().second}"
+    return f"shell screencap /sdcard/{imageName}.png"
+
+def getImage(image_name:str, result_folder:str):
+    return f"pull /sdcard/{image_name} {result_folder}"
+
--- src/tool.py
+++ src/tool.py
@@ -1,23 +1,27 @@
 import sys
 import os
+import numpy as np
+from sewar.full_ref import uqi
+from PIL import Image
 from time import sleep
 from commands import *
 
 root = os.getcwd()
 
-def sendCommand(command:str):
+def sendCommand(command:str, result:list, result_folder:str):
 
     '''
     Different treatment for each
     command.
     '''   
 
-    sentCommand = "adb shell "
+    sentCommand = "adb "
 
     if command.find("TAP") > -1:
         x = command[command.find("(") + 1:command.find(",")].strip()
         y = command[command.find(",") + 1:command.find(")")].strip()
         sentCommand += tap(x, y)
+        result.append("Pass")
 
     elif command.find("SWIPE") > -1:
 
@@ -39,6 +43,8 @@ def sendCommand(command:str):
             y2_position = command.find(",", x2_position) + 1
             y2 = command[y2_position:command.find(",", y2_position)].strip()
             sentCommand += swipe(x1, y1, x2, y2)
+        
+        result.append("Pass")
 
     elif command.find("DRAGDROP") > -1:
         x1 = command[command.find("(") + 1:command.find(",")].strip()
@@ -47,10 +53,45 @@ def sendCommand(command:str):
         y2 = command[command.find(x2) + (len(x2) + 1):command.find(",", command.find(x2) + (len(x2) + 1))].strip()
         duration = command[command.find(y2) + (len(y2) + 1):command.find(")", command.find(y2) + (len(y2) + 1))].strip()
         sentCommand += dragAndDrop(x1, y1, x2, y2, duration)
+        result.append("Pass")
 
     elif command.find("OPEN") > -1:
         app = command[command.find("(") + 1:command.find(")")].strip()
         sentCommand += openApp(app)
+        result.append("Pass")
+
+    elif command.find("COMPARE_IMAGE") > -1:
+        if not os.path.exists(f"{root}/reference"):
+            os.mkdir(f"{root}/reference")
+            result.append("Error")
+            return
+
+        referenceImage = command[command.find("(") + 1:command.find(",")].strip()
+        passRate = float(command[command.find(",") + 1:command.find(")")].strip())
+
+        if not os.path.exists(f"{root}/reference/{referenceImage}"):
+            result.append("Error")
+            return
+
+        sentCommand_ = sentCommand + captureImage()
+        validationImage = sentCommand_[sentCommand_.find("/", sentCommand_.find("sdcard")) + 1:]
+
+        os.system(sentCommand_)        
+
+        sentCommand_ = sentCommand + getImage(validationImage, result_folder)
+
+        os.system(sentCommand_)
+
+        original = np.array(Image.open(f"{result_folder}/{validationImage}"))
+        reference = np.array(Image.open(f"{root}/reference/{referenceImage}"))
+
+        comparison = uqi(original, reference) * 100
+        
+        if comparison >= passRate:
+            result.append("Pass")
+        else:
+            result.append("Fail")
+        return
 
     # Set sleep between tests
     if command.find("{") > -1:
@@ -86,7 +127,7 @@ def getCommands(test_case:str):
             with open(f"{root}/test-cases/{test_case}.tca", "r") as test_caseCommads:
                 commands = []
                 for command in test_caseCommads.readlines():
-                    if command[1] != "#":
+                    if command[0] != "#":
                         commands.append(command.strip("\n"))
                 return commands
 
@@ -114,13 +155,31 @@ def getTestCases(test_cycle:str):
 
 def execTestCycle(test_cycle:str):
 
-    for testCase in getTestCases(test_cycle):
-        logging = f"Executing '{testCase}'"             
-        for command in getCommands(testCase):            
-            sendCommand(command)            
-            logging += "."
-            sys.stdout.write("\r")
-            sys.stdout.write(logging)
-            sys.stdout.flush()            
-        print(f" Complete")
+    # Create folder for storage results
+    if not os.path.exists(f"{root}/test-execution"):
+            os.mkdir(f"{root}/test-execution")
+    
+    result_folder = f"{root}/test-execution/{datetime.now().day}{datetime.now().month}{datetime.now().year}{datetime.now().hour}{datetime.now().minute}{datetime.now().second}"
+    os.mkdir(result_folder)
 
+    # Start execution
+    for testCase in getTestCases(test_cycle):
+        testCase_results = f"{result_folder}/{testCase}"
+        os.mkdir(testCase_results)
+        logging = f"Executing '{testCase}'"
+        results = []
+        for command in getCommands(testCase):
+            if command != "":                
+                sendCommand(command, results, testCase_results)            
+                logging += "."
+                sys.stdout.write("\r")
+                sys.stdout.write(logging)
+                sys.stdout.flush()
+        if "Fail" in results:
+            print(f" Fail")
+        elif "Error" in results:
+            print(f" Error")
+        else:
+            print(f" Pass")
+
+execTestCycle("sample")
